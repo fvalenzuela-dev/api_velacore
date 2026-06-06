e3797be9f3b8ec104300806eff2f50e9f8be99d3
kuroro <fernandovalenzuelaibarra@gmail.com>
2026-06-05 21:26:49 -0400

    fix(codacy): move market data constants

--- src/api_velacore/infrastructure/market_data.py
+++ src/api_velacore/infrastructure/market_data.py
@@ -19,73 +19,26 @@ from api_velacore.schemas.market_data import (
     TwelveDataStocksResponse,
 )
 
+from .constants import (
+    BINANCE_INTERVALS as BINANCE_INTERVALS,
+)
+from .constants import (
+    TWELVE_DATA_ASSET_TYPES as TWELVE_DATA_ASSET_TYPES,
+)
+from .constants import (
+    TWELVE_DATA_INTERVALS as TWELVE_DATA_INTERVALS,
+)
+from .constants import (
+    TWELVE_DATA_TYPE_BY_ASSET,
+)
+from .constants import (
+    YAHOO_INTERVALS as YAHOO_INTERVALS,
+)
+from .constants import (
+    YAHOO_PERIODS as YAHOO_PERIODS,
+)
 from .errors import MarketDataProviderError as MarketDataProviderError
 from .normalization import normalize_binance_exchange_symbol
-
-BINANCE_INTERVALS = frozenset(
-    {
-        "1s",
-        "1m",
-        "3m",
-        "5m",
-        "15m",
-        "30m",
-        "1h",
-        "2h",
-        "4h",
-        "6h",
-        "8h",
-        "12h",
-        "1d",
-        "3d",
-        "1w",
-        "1M",
-    }
-)
-
-YAHOO_INTERVALS = frozenset(
-    {
-        "1m",
-        "2m",
-        "5m",
-        "15m",
-        "30m",
-        "60m",
-        "90m",
-        "1h",
-        "1d",
-        "5d",
-        "1wk",
-        "1mo",
-        "3mo",
-    }
-)
-
-YAHOO_PERIODS = frozenset(
-    {"1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"}
-)
-
-TWELVE_DATA_INTERVALS = frozenset(
-    {
-        "1min",
-        "5min",
-        "15min",
-        "30min",
-        "45min",
-        "1h",
-        "2h",
-        "4h",
-        "1day",
-        "1week",
-        "1month",
-    }
-)
-
-TWELVE_DATA_ASSET_TYPES = frozenset({"stock", "etf"})
-TWELVE_DATA_TYPE_BY_ASSET = {
-    "stock": "Common Stock",
-    "etf": "ETF",
-}
 
 
 @dataclass(frozen=True, slots=True)
